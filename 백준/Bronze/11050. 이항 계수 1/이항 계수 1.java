0bdf0d3dd184e3ab59f34fac8e438fae821b9485
partmant <162757736+partmant@users.noreply.github.com>
2025-08-28 20:48:37 +0900

    [Bronze I] Title: 이항 계수 1, Time: 172 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt(), K = sc.nextInt();
+		int N = sc.nextInt();
+		int K = sc.nextInt();
 
-		if (N >> 1 < K)
-			K = N - K;
+		int[][] dp = new int[N + 1][N + 1];
 
-		int num = 1, denum = 1;
-
-		while (K > 0) {
-			num *= N--;
-			denum *= K--;
+		for (int i = 0; i < N + 1; i++) {
+			dp[i][0] = 1;
+			dp[i][1] = i;
+			dp[i][i] = 1;
 		}
 
-		System.out.println(num / denum);
+		for (int i = 2; i <= N; i++)
+			for (int j = 1; j < i; j++)
+				dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+
+		System.out.println(dp[N][K]);
 	}
 }
