4542ec8f2240f9a6a163571970d1a5616bc58d15
partmant <162757736+partmant@users.noreply.github.com>
2025-12-03 15:27:34 +0900

    [Silver II] Title: 키로거, Time: 1040 ms, Memory: 177956 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        while (T-- > 0) {
+            String line = br.readLine();
+            
+            Stack<Character> left = new Stack<>();
+            Stack<Character> right = new Stack<>();
+            
+            for (char c : line.toCharArray()) {
+                if (c == '<') {
+                    if (!left.isEmpty()) {
+                        right.push(left.pop());
+                    }
+                } 
+                else if (c == '>') {
+                    if (!right.isEmpty()) {
+                        left.push(right.pop());
+                    }
+                } 
+                else if (c == '-') {
+                    if (!left.isEmpty()) {
+                        left.pop();
+                    }
+                } 
+                else {
+                    left.push(c);
+                }
+            }
+            
+            while (!left.isEmpty()) {
+                right.push(left.pop());
+            }
+            while (!right.isEmpty()) {
+                sb.append(right.pop());
+            }
+            sb.append('\n');
+        }
+        
+        System.out.print(sb.toString());
+    }
+}
