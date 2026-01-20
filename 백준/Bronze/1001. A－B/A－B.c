8916a913ec72562caa7ba2a70176250f78f42131
partmant <162757736+partmant@users.noreply.github.com>
2026-01-20 16:44:02 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.c
@@ -0,0 +1,19 @@
+/*
+문제: A-B 출력
+작성자:홍창희
+작성일:2020.4.17
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int A, B;
+    
+    scanf("%d%d", &A, &B);
+    printf("%d", A-B);
+    
+    return 0;
+}
+    
+   
