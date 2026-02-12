3d1407bb7c8af81b2d37f8118a3f1fadd1b9391d
partmant <162757736+partmant@users.noreply.github.com>
2026-02-12 22:24:37 +0900

    [Gold IV] Title: 네트워크 연결, Time: 516 ms, Memory: 49432 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class Edge implements Comparable<Edge> {
+        int from;
+        int to;
+        int cost;
+
+        Edge(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    static int[] parent;
+
+    static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static boolean union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if (rootA == rootB) return false;
+
+        parent[rootB] = rootA;
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        List<Edge> edges = new ArrayList<>();
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            edges.add(new Edge(a, b, c));
+        }
+
+        Collections.sort(edges);
+
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        int result = 0;
+        int count = 0;
+
+        for (Edge edge : edges) {
+            if (union(edge.from, edge.to)) {
+                result += edge.cost;
+                count++;
+
+                if (count == N - 1) break;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
