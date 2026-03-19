019ba85303730903249fac25c508253c463cd8f7
partmant <162757736+partmant@users.noreply.github.com>
2026-03-19 23:32:29 +0900

    [Gold IV] Title: 타일 채우기, Time: 172 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        if (N % 2 == 1) {
+            System.out.println(0);
+            return;
+        }
+
+        int[] dp = new int[N + 1];
+        dp[0] = 1;
+
+        for (int i = 2; i <= N; i += 2) {
+            dp[i] = dp[i - 2] * 3;
+
+            for (int j = i - 4; j >= 0; j -= 2) {
+                dp[i] += dp[j] * 2;
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
