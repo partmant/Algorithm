51ab848b931c29e4fb14a11dfa563eb1ca36bd84
partmant <162757736+partmant@users.noreply.github.com>
2026-03-20 18:44:04 +0900

    [Gold V] Title: 퇴사 2, Time: 676 ms, Memory: 315836 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,59 +1,30 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-	static class Counsel {
-		int startDate;
-		int price;
+        int[] T = new int[N + 1];
+        int[] P = new int[N + 1];
+        int[] dp = new int[N + 2];
 
-		Counsel(int startDate, int price) {
-			this.startDate = startDate;
-			this.price = price;
-		}
-	}
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+        for (int i = N; i >= 1; i--) {
+            if (i + T[i] - 1 <= N) {
+                dp[i] = Math.max(dp[i + 1], P[i] + dp[i + T[i]]);
+            } else {
+                dp[i] = dp[i + 1];
+            }
+        }
 
-		ArrayList<Counsel>[] days = new ArrayList[N + 1];
-		for (int i = 0; i <= N; i++) {
-			days[i] = new ArrayList<>();
-		}
-
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int T = Integer.parseInt(st.nextToken());
-			int P = Integer.parseInt(st.nextToken());
-
-			if (i + T <= N) {
-				days[i + T].add(new Counsel(i + 1, P));
-			}
-		}
-
-		int dp[] = new int[N + 1];
-
-		for (int i = 1; i <= N; i++) {
-			int max = dp[i - 1];
-			ArrayList<Counsel> day = days[i];
-
-			if (day.isEmpty()) {
-				dp[i] = max;
-				continue;
-			}
-
-			for (int j = 0; j < day.size(); j++) {
-				Counsel counsel = day.get(j);
-				int prev = counsel.startDate - 1;
-				int price = counsel.price;
-				max = Math.max(max, dp[prev] + price);
-			}
-			dp[i] = max;
-		}
-
-		System.out.println(dp[N]);
-	}
+        System.out.println(dp[1]);
+    }
 }
