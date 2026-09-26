c78b5142522aeea322270eda2e972716677241e3
partmant <162757736+partmant@users.noreply.github.com>
2026-09-26 13:31:55 +0900

    [level 2] Title: 괄호 회전하기, Time: 17.45 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (isCorrect(s.substring(i) + s.substring(0, i))) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    private boolean isCorrect(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c == '[' || c == '{' || c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                
+                char top = stack.pop();
+                if (c == ']' && top != '[') return false;
+                if (c == '}' && top != '{') return false;
+                if (c == ')' && top != '(') return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
